7cb653d8804d605acdba5dfe033a35ecd6ace7f0
ian-frederickabondo <ian-frederickabondo@ext-130-66-207-77-wifi.ec-nantes.fr>
2025-09-19 09:22:11 +0200

    creatio classe point2D

---
+++ testgithub/src/main/java/edu/centralenantes/testgithub/point2Dtest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.centralenantes.testgithub;
+
+/**
+ *
+ * @author ian-frederickabondo
+ */
+public class point2Dtest {
+    
+}
